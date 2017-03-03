8d0b6867be04160ca703b9f0cea99dced0141cb0
felipemillhouse <felipemillhouse@gmail.com>
2017-03-03 01:02:53 -0300

    a

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@
 // para rodar a aplicação em modo dev, basta entrar na pasta do app pelo terminal e digitar npm run dev
 //
 const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://localhost/db_finance')
+//module.exports = mongoose.connect('mongodb://localhost/db_finance')
+module.exports = mongoose.connect('mongodb://user4b456f:e58da0DJkS1700410bd@cluster-pgrs2-0-eu-west-1-scalabledbs.cloudstrap.io:27002,cluster-pgrs2-1-eu-west-1-scalabledbs.cloudstrap.io:27002/pg-app-zgvh30jlhxbw5qbbkvwxs7894wfyuw?replicaSet=pgrs2&ssl=true')
 
 //Traduções de mensagens de erro
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
