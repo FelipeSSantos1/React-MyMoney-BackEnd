399737fe389df07004660fc3049c36cf6a828df8
felipemillhouse <felipemillhouse@gmail.com>
2017-03-12 17:11:17 -0300

    Start

--- billingCycle/billingCycleService.js
+++
@@ -1,5 +0,0 @@
-const billingCycle = require('billingCycle')
-
-billingCycle.methods(['get', 'post', 'put', 'delete'])
-
-module.exports = billingCycle
--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,4 @@
-const port = 4004
+const port = 3003
 
 const bodyParser = require('body-parser')
 const express = require('express')
@@ -6,13 +6,13 @@ const server = express()
 const allowCors = require('./cors')
 const queryParser = require('express-query-int')
 
-server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
 server.use(queryParser())
 
-server.listen(port, function(){
-  console.log(`BEACKEND is running on port ${port}.`)
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
 })
 
 module.exports = server
